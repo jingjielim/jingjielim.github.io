02719364bbc650daed8afead5aa940b9f898946b
jingjielim <jingjie3000@gmail.com>
2020-03-21 19:41:00 -0400

    Add simple typing carousel
    Update README to include credits for templates

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Portfolio
+
+Jing Jie Lim's personal portfolio site
+
+# Credits
+- [Base template](https://startbootstrap.com/themes/creative/)
+- [Simple Typing Carousel](https://codepen.io/Braunson/pen/tilaw)
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -53,4 +53,66 @@ $(() => {
   //     tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
   //   }
   // })
+  // function takes in a html element, an object called toRotate, and a period in milliseconds
+  function txtRotate (el, toRotate, period = 2000) {
+    const txt = ''
+    const isDeleting = false
+    const completed = false
+    const delta = 70 - Math.random() * 20
+    const i = 0
+    const fullTxt = toRotate[0]
+    const lastTxt = toRotate[toRotate.length - 1]
+    tick(el, isDeleting, txt, fullTxt, lastTxt, completed, delta, period, i, toRotate)
+  }
+
+  function tick (el, isDeleting, txt, fullTxt, lastTxt, completed, delta, period, i, toRotate) {
+    if (isDeleting) {
+      txt = fullTxt.substring(0, txt.length - 1)
+    } else {
+      txt = fullTxt.substring(0, txt.length + 1)
+    }
+    if (txt === lastTxt) {
+      completed = true
+    }
+    if (isDeleting) { delta = 50 }
+
+    if (!isDeleting && txt === fullTxt) {
+      delta = period
+      isDeleting = true
+    } else if (isDeleting && txt === '') {
+      isDeleting = false
+      i++
+      fullTxt = toRotate[i%3]
+      delta = 70 - Math.random() * 20
+    }
+    if (txt === fullTxt) {
+      el.innerHTML = txt + '<span class="blink">|</span>'
+    } else {
+      el.innerHTML = txt + '<span class="no-blink">|</span>'
+    }
+
+    setTimeout(function () {
+      if (!completed) {
+        tick(el, isDeleting, txt, fullTxt, lastTxt, completed, delta, period, i, toRotate)
+      }
+    }, delta)
+  }
+
+  window.onload = function () {
+    // find all the elements with class txt-rotate
+    const elements = $('.txt-rotate')
+    // for each of the elements
+    for (let i = 0; i < elements.length; i++) {
+      // initialise variable toRotate with string in the data-rotate attribute
+      const toRotate = elements[i].getAttribute('data-rotate')
+      // initialise variable period with string in the data-period attribute
+      const period = elements[i].getAttribute('data-period')
+      // if data-rotate attribute is not empty
+      if (toRotate) {
+        // call the function TxtRotate, passing in the element, the data-rotate attribute parsed as a JS object, and the period
+        txtRotate(elements[i], JSON.parse(toRotate), parseInt(period))
+        // new TxtRotate(elements[i], JSON.parse(toRotate), period)
+      }
+    }
+  }
 })
